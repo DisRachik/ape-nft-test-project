6d0b763ce8c944866f97a94895e9c3f154175924
DisRachik <radchenkoig@gmail.com>
2024-03-22 17:01:09 +0200

    init: deploy sterting temlate

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	base: "/ape-nft-test-project/",
+	plugins: [react()],
+	build: {
+		sourcemap: true,
+	},
+	resolve: {
+		alias: {
+			src: "/src",
+			components: "/src/components",
+		},
+	},
+});
