48b1795df83e9a8bbbf5bae83628b9f21a1d8d13
DisRachik <radchenkoig@gmail.com>
2024-03-23 19:20:06 +0200

    feat: customize build and add images, icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "src/icons/logo.svg";
+import viteLogo from "src/icons/logo.svg";
+import Logo from "src/icons/meta-mask-color.svg?react";
+import "./App.css";
+
+import imgDtPng from "src/images/hero/monkey-hero-dt.png";
+import imgDtPng2 from "src/images/hero/monkey-hero-dt@2x.png";
+import imgDt from "src/images/webp/monkey-hero-dt.webp";
+import imgDt2 from "src/images/webp/monkey-hero-dt@2x.webp";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<div>
+				<Logo />
+				<a href="https://vitejs.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+
+			<picture>
+				<source srcSet={`${imgDt} 1x, ${imgDt2} 2x`} type="image/webp" media="(min-width: 768px)" />
+				<source srcSet={`${imgDtPng} 1x, ${imgDtPng2} 2x`} type="image/png" media="(min-width: 768px)" />
+				<img src={imgDtPng} alt="S’mores Frappuccino" width="480" height="480" loading="lazy" />
+			</picture>
+
+			<h1>Vite + React</h1>
+			<div className="card">
+				<button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+				<p>
+					Edit <code>src/App.jsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+		</>
+	);
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import imagemin from "imagemin";
+import imageminWebp from "imagemin-webp";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "/ape-nft-test-project/",
-	plugins: [react()],
+	plugins: [
+		react(),
+		svgr(),
+		ViteImageOptimizer({
+			png: {
+				quality: 86,
+			},
+			jpeg: {
+				quality: 86,
+			},
+			jpg: {
+				quality: 86,
+			},
+		}),
+		{
+			...imagemin(["./src/images/**/*.{jpg,png,jpeg}"], {
+				destination: "./src/images/webp",
+				plugins: [imageminWebp({ quality: 86 })],
+			}),
+			apply: "serve",
+		},
+	],
 	build: {
 		sourcemap: true,
 	},
